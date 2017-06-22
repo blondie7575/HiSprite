3fd63c71d4a9304349f46fd47b6ef2335baadfa3
Rob McMullen <feedback@playermissile.com>
2017-06-22 06:24:39 -0700

    Added separate makefile for my new tests

---
+++ bwtest.s
@@ -0,0 +1,74 @@
+    *= $6000
+
+; os memory map
+CLRTEXT = $c050
+SETTEXT = $c051
+CLRMIXED = $c052
+SETMIXED = $c053
+TXTPAGE1 = $c054
+TXTPAGE2 = $c055
+CLRHIRES = $c056
+SETHIRES = $c057
+
+; ROM entry points
+COUT = $fded
+ROMWAIT = $fca8
+
+; Zero page locations we use (unused by Monitor, Applesoft, or ProDOS)
+PARAM0          = $06
+PARAM1          = $07
+PARAM2          = $08
+PARAM3          = $09
+SCRATCH0        = $19
+SCRATCH1        = $1a
+SPRITEPTR_L     = $1b
+SPRITEPTR_H     = $1c
+
+start   
+    bit CLRTEXT     ; start with HGR page 1, full screen
+    bit CLRMIXED
+    bit TXTPAGE1
+    bit SETHIRES
+
+    jmp draw
+
+    jsr clrscr
+
+    ldx #0
+loop
+    txa
+    sta $2000,x
+    inx
+    bne loop
+
+draw
+    lda #100
+    sta PARAM1 ; y coord
+    lda #100
+    sta PARAM0 ; x coord
+
+    jsr APPLE
+
+wait
+    jmp wait
+
+
+clrscr
+    lda #0
+    sta clr1+1
+    lda #$20
+    sta clr1+2
+clr0
+    lda #$81
+    ldy #0
+clr1
+    sta $ffff,y
+    iny
+    bne clr1
+    inc clr1+2
+    ldx clr1+2
+    cpx #$40
+    bcc clr1
+    rts
+
+    .include apple.s
